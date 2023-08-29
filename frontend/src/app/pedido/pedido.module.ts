92fb32747dfca010a7b5fce39e16c04549b63ab3
Lídia Gomes <lidia.gomes@ufpr.br>
2023-08-28 23:48:55 -0300

    Remoção da página visualizar conteúdo e edição da página listar produto

--- frontend/src/app/pedido/pedido.module.ts
+++ frontend/src/app/pedido/pedido.module.ts
@@ -4,14 +4,12 @@ import { RouterModule } from '@angular/router';
 
 import { InserirPedidoComponent } from './inserir-pedido/inserir-pedido.component';
 import { PesquisarPedidoComponent } from './pesquisar-pedido/pesquisar-pedido.component';
-import { VisualizarPedidoComponent } from './visualizar-pedido/visualizar-pedido.component';
 import { ModalVisualizarPedidoComponent } from './modal-visualizar-pedido/modal-visualizar-pedido.component';
 
 @NgModule({
   declarations: [
     InserirPedidoComponent,
     PesquisarPedidoComponent,
-    VisualizarPedidoComponent,
     ModalVisualizarPedidoComponent
   ],
   imports: [
