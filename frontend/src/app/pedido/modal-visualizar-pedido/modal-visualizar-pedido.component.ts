78e29c327603648e89bd0f3d8d856892695bd2e6
dbs1507 <danielbloch15@gmail.com>
2023-09-19 21:47:25 -0300

    implementacao de localStorage no pedido

--- frontend/src/app/pedido/modal-visualizar-pedido/modal-visualizar-pedido.component.ts
+++ frontend/src/app/pedido/modal-visualizar-pedido/modal-visualizar-pedido.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap'
+import { PedidoService } from "../services/pedido.service";
+import { Pedido } from "../../shared/models/pedido.model";
 
 @Component({
   selector: 'app-modal-visualizar-pedido',
@@ -13,4 +14,21 @@ export class ModalVisualizarPedidoComponent {
   constructor(public activeModal: NgbActiveModal) {}
   ngOnInit(): void {
   }
+
+  //acredito que o codigo abaixo tbm vai ser necessario para implementar a visualizacao 
+  // export class ModalVisualizarPedidoComponent implements OnInit {
+  //   @Input() pedidoId: number; // Receba o ID do pedido como entrada
+  //   pedido: Pedido | undefined;
+    
+  //   constructor(public activeModal: NgbActiveModal, private pedidoService: PedidoService) {}
+  
+  //   ngOnInit(): void {
+  //     // No ngOnInit, chame a função buscarPedidoPorId para obter o pedido com base no ID
+  //     this.buscarPedidoPorId(this.pedidoId);
+  //   }
+  
+  //   buscarPedidoPorId(id: number) {
+  //     this.pedido = this.pedidoService.buscarPorId(id.toString()); 
+  //   }
+  // }
 }
